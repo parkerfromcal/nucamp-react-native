c061d352e049686657098ea8c70cda7bc6cb44bb
parkerfromcal <kparker.adrienne@gmail.com>
2021-05-15 10:43:23 -0400

    feat: contact card

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import { ScrollView } from "react-native-gesture-handler";
+import { Text } from "react-native";
+import { Card } from "react-native-elements";
 
 class Contact extends Component {
   static navigationOptions = {
     title: "Contact Us",
   };
   render() {
-    return <ScrollView></ScrollView>;
+    return (
+      <ScrollView>
+        <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
+          <Text>1 Nucamp Way</Text>
+          <Text>Seattle, WA 98001</Text>
+          <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+          <Text>Phone: 1-206-555-1234</Text>
+          <Text style={{ marginBottom: 10 }}>Email: campsites@nucamp.co</Text>
+        </Card>
+      </ScrollView>
+    );
   }
 }
 
